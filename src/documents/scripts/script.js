2200f442eec9057a062a21b7da41829b724df6e5
Jason Lally <jason.lally@gmail.com>
2013-12-24 15:52:41 -0800

    debug scrolling, fix tracking of current position, add arrows

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -1,27 +1,31 @@
 $(function() {
   var moveSlide = true;
   var sectionMap = {};
-  var currentSection = $('#missing-you');
+  var currentSection = $('#beginning');
   var next, prev;
-  $('section').each(function(i,obj){
-    console.log($(obj));
+
+  $.localScroll({
+    onBefore: function(ev, el, scroll) {
+      currentSection = $(el);
+    }
   });
 
   $('body').on('mousewheel', function(event) {
     event.preventDefault();
     if(moveSlide) {
-      if(event.deltaY < -2) {
+      if(event.deltaY < 0 && currentSection[0] != $("#end")[0]) {
         next = nextSection(currentSection);
-        $.scrollTo(next, 1000);
+        $.scrollTo(next, 850);
         currentSection = next;
         moveSlide = false;
-      } if(event.deltaY > 2) {
+        setTimeout(function(){ moveSlide = true; }, 1300);
+      } if(event.deltaY > 0 && currentSection[0] != $("#beginning")[0]) {
         prev = prevSection(currentSection);
-        $.scrollTo(prev, 1000);
+        $.scrollTo(prev, 850);
         currentSection = prev;
         moveSlide = false;
+        setTimeout(function(){ moveSlide = true; }, 1300);
       }
-    setTimeout(function(){ moveSlide = true; }, 1300);
     }
   });
 
@@ -30,7 +34,7 @@ $(function() {
   }
 
   var prevSection = function(section) {
-    return $(section).prev();
+    return section.prev();
   }
 });
 
