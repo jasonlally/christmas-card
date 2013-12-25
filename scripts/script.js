340cfd96b886c7cd6c9c3c3a45a946c702888bec
Jason Lally <jason.lally@gmail.com>
2013-12-24 22:29:04 -0800

    912b37c change title

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+$(function() {
+  var moveSlide = true;
+  var sectionMap = {};
+  var currentSection = $('#beginning');
+  var next, prev;
+
+  $.localScroll({
+    onBefore: function(ev, el, scroll) {
+      currentSection = $(el);
+    }
+  });
+
+  $('body').on('mousewheel', function(event) {
+    event.preventDefault();
+    if(moveSlide) {
+      if(event.deltaY < 0 && currentSection[0] != $("#end")[0]) {
+        next = nextSection(currentSection);
+        $.scrollTo(next, 850);
+        currentSection = next;
+        moveSlide = false;
+        setTimeout(function(){ moveSlide = true; }, 1300);
+      } if(event.deltaY > 0 && currentSection[0] != $("#beginning")[0]) {
+        prev = prevSection(currentSection);
+        $.scrollTo(prev, 850);
+        currentSection = prev;
+        moveSlide = false;
+        setTimeout(function(){ moveSlide = true; }, 1300);
+      }
+    }
+  });
+
+  var nextSection = function(section) {
+    return section.next();
+  };
+
+  var prevSection = function(section) {
+    return section.prev();
+  };
+  $("#beginning .inner").backstretch("../images/family_dinner.jpg");
+  $("#second .inner").backstretch("../images/goldengate.jpg");
+  $("#third .inner").backstretch("../images/christmas_lights.jpg");
+});
+
